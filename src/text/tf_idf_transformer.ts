be96bf66651def6cef85b6a17e31d706defb3a64
NeTT <nett@nett.moe>
2023-06-29 14:35:23 +0530

    use direct multiplication instead of matrices

--- src/text/tf_idf_transformer.ts
+++ src/text/tf_idf_transformer.ts
@@ -13,24 +13,9 @@ export function countFrequency(data: ArrayLike<number>[]): Float32Array {
   return freq;
 }
 
-/** Construct a sparse matrix from given diagonals. */
-export function spFromDiags(
-  diags: ArrayLike<number>,
-  size: number,
-): Float32Array[] {
-  const res = new Array(size);
-  let i = 0;
-  while (i < size) {
-    res[i] = new Float32Array(size);
-    res[i][i] = diags[i];
-    i += 1;
-  }
-  return res;
-}
-
 /** Convert tf features (CountVectorizer) into tf-idf features. */
 export class TfIdfTransformer {
-  idf: null | Float32Array[];
+  idf: null | Float32Array;
   constructor() {
     this.idf = null;
   }
@@ -49,33 +34,27 @@ export class TfIdfTransformer {
       idf[i] = Math.log(shape.samples / freq[i]) + 1;
       i += 1;
     }
-    this.idf = spFromDiags(idf, shape.features);
+    this.idf = idf;
     return this;
   }
   /** 
    * Transform an tf features into tf-idf features. 
-   * Warning: Currently extremely slow. If you have a fast matrix multiplication
-   * method, consider using that to multiply `data` and `TfIdfTransformer.idf`.
    */
   transform(data: ArrayLike<number>[]): Float32Array[] {
     if (this.idf === null) throw new Error("IDF not initialized yet.");
-    return matMul(data, this.idf);
+    return multiplyDiags(data, this.idf);
   }
 }
 
-/** A very basic, low-effort matrix multiplication. */
-export function matMul(x: ArrayLike<number>[], y: ArrayLike<number>[]): Float32Array[] {
+/** A very basic, low-effort multiplication. */
+export function multiplyDiags(x: ArrayLike<number>[], y: ArrayLike<number>): Float32Array[] {
   const res = new Array(x.length);
   let i = 0;
   while (i < x.length) {
     res[i] = new Float32Array(y.length)
     let j = 0;
     while (j < y.length) {
-      let k = 0;
-      while (k < y.length) {
-        res[i][j] += x[i][k] * y[k][j];
-        k += 1;
-      }
+      res[i][j] = x[i][j] * y[j]
       j += 1;
     }
     i += 1;
