d27a5e7b4318871fc4790256b96917f8e7dee55c
NeTT <nett@nett.moe>
2023-07-14 23:44:31 +0530

    switching to a different dir structure

---
+++ src/text.ts
@@ -0,0 +1,2 @@
+export { TfIdfTransformer } from "./text/tf_idf_transformer.ts";
+export { CountVectorizer } from "./text/count_vectorizer.ts";
--- src/text/mod.ts
+++
@@ -1,3 +0,0 @@
-export { TfIdfTransformer } from "./tf_idf_transformer.ts";
-
-export { CountVectorizer } from "./count_vectorizer.ts";
