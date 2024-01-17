70bd9ef6c7cf3aa3b4e8e2a2adbc799a9ec8b796
NeTT <nett@nett.moe>
2024-01-17 20:08:34 +0530

    add untransform method

--- src/encoding/categorical.ts
+++ src/encoding/categorical.ts
@@ -25,10 +25,10 @@ export class CategoricalEncoder<T> {
   }
   /** One-hot encoding of categorical values */
   transform<DT extends TypedArray>(targets: T[], dType: DataType): Matrix<DT> {
-    const data = new (getConstructor(dType))(this.#lastToken[0] * targets.length)
-    const res = new Matrix<DT>(data as DT, [
-      this.#lastToken[0]
-    ]);
+    const data = new (getConstructor(dType))(
+      this.#lastToken[0] * targets.length
+    );
+    const res = new Matrix<DT>(data as DT, [targets.length]);
     let i = 0;
     while (i < targets.length) {
       const index = this.mapping.get(targets[i]);
@@ -40,6 +40,22 @@ export class CategoricalEncoder<T> {
       i += 1;
     }
     return res;
+  }
+  untransform<DT extends TypedArray>(data: Matrix<DT>): T[] {
+    const res = new Array(data.nRows);
+    for (let i = 0; i < res.length; i += 1) {
+      const idx = data.row(i).findIndex((x) => x === 1);
+      res[i] = this.getOg(idx) || "__unknown__";
+    }
+    return res;
+  }
+  getOg(data: number): T | undefined {
+    for (const [k, v] of this.mapping.entries()) {
+      if (v === data) {
+        return k;
+      }
+    }
+    return undefined;
   }
   #incrementToken(): number {
     return Atomics.add(this.#lastToken, 0, 1);
