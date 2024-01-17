cdb731fe66d8c39537f1686a0ea848d7ea532d48
NeTT <nett@nett.moe>
2024-01-17 23:01:42 +0530

    forgot to export encoding

--- src/mod.ts
+++ src/mod.ts
@@ -2,4 +2,5 @@ export * from "./feature/text.ts";
 export * from "./feature/image.ts";
 export * from "./utils/mod.ts";
 export * from "./metrics/mod.ts"
+export * from "./encoding/mod.ts"
 export type * from "./feature/types.ts";
